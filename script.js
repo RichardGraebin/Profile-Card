3735bc9b8cd12dd8abb54cbab7ec74b9e656cd93
RichardGraebin <richardgraebinjs@gmail.com>
2021-03-13 18:41:29 -0300

    animation pt1

---
+++ script.js
@@ -0,0 +1,13 @@
+let txt = document.getElementById('gtxt')
+
+function anOut(){
+    txt.style.opacity = 0
+    txt.style.visibility = 'hidden'
+
+}
+
+function anOver(){
+    txt.style.opacity = 1
+    txt.style.visibility = 'visible'
+
+}
