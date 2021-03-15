b65c8b120afd7aeaa583d2328fc99168d38a60b8
RichardGraebin <richardgraebinjs@gmail.com>
2021-03-15 12:50:08 -0300

    divimg and gimg animations

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let txt = document.getElementById('gtxt')
+let img = document.getElementById('gimg')
+
+function anOver(){
+    txt.style.opacity = 1
+    txt.style.visibility = 'visible'
+}
 
 function anOut(){
     txt.style.opacity = 0
@@ -6,8 +12,11 @@ function anOut(){
 
 }
 
-function anOver(){
-    txt.style.opacity = 1
-    txt.style.visibility = 'visible'
-
+function imgOver() {
+    img.style.transform = 'translateX(-48px)'
 }
+
+function imgOut() {
+    img.style.transform = 'translateX(0px)'
+}
+
